bc70996a71fc5c6646b46736262c1703b51a89e2
GUS <gus@belomor.nl>
2022-04-11 00:25:52 +0200

    Update uptime_display.sh

--- uptime_display.sh
+++ uptime_display.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 let upSeconds="$(/usr/bin/cut -d. -f1 /proc/uptime)"
 let secs=$((${upSeconds}%60))
 let mins=$((${upSeconds}/60%60))
@@ -7,3 +7,7 @@ let days=$((${upSeconds}/86400))
 UPTIME=`printf "%d days, %02dh%02dm%02ds" "$days" "$hours" "$mins" "$secs"`
 
 echo "Uptime: ${UPTIME}"
+
+###OUTPUT SAMPLE: Uptime: 0 days, 00h08m47s
+###OUTPUT SAMPLE: Uptime: 2 days, 02h56m06s
+
